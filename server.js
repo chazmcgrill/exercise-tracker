bc7b5e717207363e54b6c114d95c3b4fb3d38101
chazmcgrill <charlietaylordev@gmail.com>
2018-07-30 19:01:12 +0100

    Added Flex & Media Query

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const UserSchema = new mongoose.Schema({
   username: {
